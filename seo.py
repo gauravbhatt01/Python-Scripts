d3f459584b6db1c3d6112c2168e21db769184ac6
Gaurav Bhatt <gauravdotbhatt@gmail.com>
2025-10-29 22:44:03 +0530

    Create seo.py

---
+++ seo.py
@@ -0,0 +1,12 @@
+with open("robots.txt", "w") as file:
+    file.write("Allow\n")
+
+with open("llm.txt", "w") as file:
+    file.write("SITE-")
+    
+with open("sitemap.xml", "w") as file:
+    file.write("<data>\nSome handlers</data>")
+    
+with open("webstories.xml", "w") as file:
+    file.write("<data>\https://www.ifoundyoutittylover.com/pictures-for-you-spiderman-have-fun</data>")
+
